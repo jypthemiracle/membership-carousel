b1cb9977705195e73af8d8e0ed83ad7a21fa15c5
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-28 12:38:11 +0900

    feat: index.ts to load categories

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
+import { ItemCategory } from "./entity/ItemCategory";
 
 createConnection().then(async connection => {
 
@@ -12,9 +13,18 @@ createConnection().then(async connection => {
     await connection.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
 
+    console.log("Inserting a new Category into the database...");
+    const category = new ItemCategory("ship", "blue");
+    await connection.manager.save(category);
+    console.log("Saved a new category with id: " + category.id);
+
     console.log("Loading users from the database...");
     const users = await connection.manager.find(User);
     console.log("Loaded users: ", users);
+
+    console.log("Loading categories from the database...");
+    const categories = await connection.manager.find(ItemCategory);
+    console.log("Loaded categories: ", categories);
 
     console.log("Here you can setup and run express/koa/any other framework.");
 
